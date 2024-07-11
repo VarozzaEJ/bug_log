756abdb2ba27beae8d61ade0f0eebd2df6a3548f
Evan Varozza <VarozzaEJ@gmail.com>
2024-07-11 15:10:42 -0600

    creating a bug

--- src/controllers/BugsController.js
+++ src/controllers/BugsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { bugsService } from "../services/BugsService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,20 +7,29 @@ export class BugsController extends BaseController {
     constructor() {
         super('api/bugs')
         this.router
+            .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createBug)
+            .get('', this.getBugs)
     }
 
     async createBug(request, response, next) {
         try {
+            const userId = request.userInfo.id
             const bugData = request.body
-            const user = request.userInfo
-            bugData.creatorId = user.id
-            console.log(bugData);
-            const bug = await bugsService.createBug(bugData)
-            console.log(bug);
-            response.send(bug)
+            bugData.creatorId = userId
+            const newBug = await bugsService.createBug(bugData)
+            response.send(newBug)
         } catch (error) {
             next(error)
+        }
+    }
+
+    async getBugs(request, response, next) {
+        try {
+            const bugs = await bugsService.getBugs()
+            response.send(bugs)
+        } catch (error) {
+
         }
     }
 }
--- src/services/BugsService.js
+++ src/services/BugsService.js
@@ -4,8 +4,12 @@ import { dbContext } from "../db/DbContext.js"
 class BugsService {
     async createBug(bugData,) {
         const bug = await dbContext.Bugs.create(bugData)
-
+        await bug.populate('creator')
         return (bug)
+    }
+    async getBugs() {
+        const bug = await dbContext.Bugs.find()
+        return bug
     }
 
 }
